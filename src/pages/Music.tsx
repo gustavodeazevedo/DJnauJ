262f6fd83983966e3282c687f8d4d67854b2a9b5
Gustavo de Azevedo <111658728+gustavodeazevedo@users.noreply.github.com>
2025-03-06 09:43:23 -0300

    Update Music.tsx

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -63,7 +63,7 @@ function Music() {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
               {playlists.map((playlist) => (
                 <motion.div
                   key={playlist.id}
