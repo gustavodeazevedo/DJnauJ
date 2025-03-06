cac90c7e4f27c856df9d863da2efbe59863624e2
Gustavo de Azevedo <111658728+gustavodeazevedo@users.noreply.github.com>
2025-03-06 10:10:13 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,7 @@ function Home() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.2 }}
-        className="text-5xl font-bold mb-1"
+        className="text-4xl sm:text-5xl font-bold mb-1"
       >
         DJ nauJ
       </motion.h1>
@@ -33,7 +33,7 @@ function Home() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.4 }}
-        className="text-lg text-gray-300 mt-5 mb-8"
+        className="text-md sm:text-lg text-gray-300 mt-5 mb-8"
       >
         Festas de comissão | Festas de aniversário | DJ Set
       </motion.p>
@@ -42,27 +42,28 @@ function Home() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.6 }}
-        className="flex space-x-4"
+        className="flex flex-row flex-wrap justify-center gap-2 sm:gap-4 w-full max-w-xs sm:max-w-none"
       >
         <a
           href="https://www.instagram.com/nauj.dj?igsh=ZXdtNzk4ejRkaWY="
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-spotify-green text-spotify-black px-6 py-3 rounded-full flex items-center space-x-2 hover:bg-opacity-80 transition-all duration-200"
+          className="bg-spotify-green text-spotify-black px-4 sm:px-6 py-2 sm:py-3 rounded-full flex items-center justify-center space-x-2 hover:bg-opacity-80 transition-all duration-200 text-sm sm:text-base"
         >
-          <Instagram className="h-5 w-5" />
+          <Instagram className="h-4 w-4 sm:h-5 sm:w-5" />
           <span>Instagram</span>
         </a>
+
         <a
           href="https://open.spotify.com/intl-pt/artist/7kCo73uiRFAZCeQwBPu5P6?si=F6Ypt3m7Q7WXfpACe-zxPg"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-spotify-green text-spotify-black px-6 py-3 rounded-full flex items-center space-x-2 hover:bg-opacity-80 transition-all duration-200"
+          className="bg-spotify-green text-spotify-black px-4 sm:px-6 py-2 sm:py-3 rounded-full flex items-center justify-center space-x-2 hover:bg-opacity-80 transition-all duration-200 text-sm sm:text-base"
         >
           <img
             src="https://img.icons8.com/ios/50/spotify--v1.png"
-            className="w-7"
-            alt="spotify"
+            className="w-4 sm:w-5"
+            alt="Spotify"
           />
           <span>Spotify</span>
         </a>
@@ -71,12 +72,12 @@ function Home() {
           href="https://www.youtube.com/@DJnauJ/featured"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-spotify-green text-spotify-black px-6 py-3 rounded-full flex items-center space-x-2 hover:bg-opacity-80 transition-all duration-200"
+          className="bg-spotify-green text-spotify-black px-4 sm:px-6 py-2 sm:py-3 rounded-full flex items-center justify-center space-x-2 hover:bg-opacity-80 transition-all duration-200 text-sm sm:text-base"
         >
           <img
             src="https://img.icons8.com/ios/50/youtube-play--v1.png"
             alt="YouTube"
-            className="w-7"
+            className="w-4 sm:w-5"
           />
           <span>YouTube</span>
         </a>
